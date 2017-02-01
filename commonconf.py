4a7d591e59cef6a9b95b9a78d3e93aa58a6aaa05
drummyfish <tastyfish@seznam.cz>
2017-02-01 20:20:15 +0100

    better config

---
+++ commonconf.py
@@ -0,0 +1,33 @@
+# config both for local and publish
+
+import __builtin__
+
+def set_common_variables(module_ref):
+  module_ref.AUTHOR = u'Miloslav \u010c\xed\u017e'
+
+  module_ref.SOCIAL = (
+    ('GitHub','https://github.com/drummyfish'),
+    ('Facebook','https://www.facebook.com/miloslav.ciz'),
+    ('YouTube','https://www.youtube.com/user/drummyfish'),
+    ('Deviantart','http://drummyfish.deviantart.com/'),
+    ('reddit','https://www.reddit.com/user/drummyfish/'),)
+
+  module_ref.DEFAULT_PAGINATION = False
+  module_ref.PLUGIN_PATHS = ['plugins']
+  module_ref.PLUGINS = ['jinja2content']
+  module_ref.PATH = 'content'
+  module_ref.TIMEZONE = 'Europe/Prague'
+  module_ref.DISPLAY_CATEGORIES_ON_MENU = False
+  module_ref.DEFAULT_LANG = u'cs'
+  module_ref.THEME = "./themes/tastyfish"
+  module_ref.PAGE_ORDER_BY = "date"
+  module_ref.FEED_ALL_ATOM = "all.rss.xml"
+  module_ref.CATEGORY_FEED_ATOM = None
+  module_ref.TRANSLATION_FEED_ATOM = None
+  module_ref.AUTHOR_FEED_ATOM = None
+  module_ref.AUTHOR_FEED_RSS = None
+  module_ref.RELATIVE_URLS = True
+
+  #module_ref.LINKS = (('Pelican', 'http://getpelican.com/'),
+  #       ('Python', 'http://python.org/'),
+  #       ('Jinja2', 'http://jinja.pocoo.org/'),)
